3ae41ba6569d1a452372d4afc9931e9cbf5cdf75
Mattew98 <mriddformal@outlook.com>
2026-02-23 10:54:52 +0000

    Update 1.1
    Updated code, accurate search function and optimised memory usage

--- data_writer.py
+++ data_writer.py
@@ -1,36 +1,44 @@
 import csv
 
 #Prompt
+def datawrite():
+    Path = input("Path of picture:\n")
+    Number = int(input("How many people in photo?\n"))
+    People = []
 
-Path = input("Path of picture:\n")
-Number = int(input("How many people in photo?\n"))
-People = []
+    #Creating array of people in picture
 
-#Creating array of people in picture
+    i = 0
+    while i < Number:
+        person = input("Person " + str(i)+":\n")
+        People.append(person)
+        i+=1
 
-i = 0
-while i < Number:
-    person = input("Person " + str(i)+":\n")
-    People.append(person)
-    i+=1
+    #Adding new people to database
 
-#Adding new people to database
+    with open('peopledata.csv',newline='') as data:
+        storedpeople = []
+        reader=csv.reader(data,delimiter=',')
+        for row in reader:
+            if row!= []:
+                storedpeople.append(row[1])
+        for person in People:
+            if person not in storedpeople:
+                with open('peopledata.csv','a',newline='\n') as data:
+                    writer = csv.writer(data,delimiter=',')
+                    writer.writerow([str('-')]+[person])
 
-with open('peopledata.csv',newline='') as data:
-    storedpeople = []
-    reader=csv.reader(data,delimiter=',')
-    for row in reader:
-        if row!= []:
-            storedpeople.append(row[1])
-    for person in People:
-        if person not in storedpeople:
-            with open('peopledata.csv','a',newline='\n') as data:
-                writer = csv.writer(data,delimiter=',')
-                writer.writerow([str('-')]+[person])
+    #Adding new pictures to database       
 
-#Adding new pictures to database       
+    with open('picturedata.csv','a',newline='\n') as data:
+        reader=csv.reader(data,delimiter=',')
+        writer=csv.writer(data,delimiter=',')
+        writer.writerow([Number]+[Path]+People)
 
-with open('picturedata.csv','a',newline='\n') as data:
-    reader=csv.reader(data,delimiter=',')
-    writer=csv.writer(data,delimiter=',')
-    writer.writerow([Number]+[Path]+People)
+loops = input("How many pictures to add?\n")
+
+j = 0
+
+while j<int(loops):
+    datawrite()
+    j+=1
--- searcher.py
+++ searcher.py
@@ -58,7 +58,7 @@ with open('picturedata.csv',newline='') as data:
         while i < int(row[0]):
             csvpeople.append(row[i+2])
             i+=1
-        globals()[f'picture{str(datareader.line_num)}'] = Picture(datareader.line_num,row[1],csvpeople)
+        globals()[f'picture{str(datareader.line_num)}'] = Picture(datareader.line_num-1,row[1],csvpeople)
         globalpictures.append(globals()[f'picture{str(datareader.line_num)}'])
 
 #applying pictures to people
@@ -73,32 +73,65 @@ print("Finished Loading.")
 
 #test
 
-for person in globalpeople:
-    print(person.index, person.name, person.pictures)
+#for person in globalpeople:
+#    print(person.index, person.name, person.pictures)
 
-for picture in globalpictures:
-    print(picture.index,picture.path,picture.people)
+#for picture in globalpictures:
+#    print(picture.index,picture.path,picture.people)
 
 #search function
 
-def search(pictureindices,p1,p2,path,loopnum,peoplepath):
-    if p1 not in peoplepath:
-        peoplepath.append(p1)
-        loopnum +=1
-        for picture in globalpictures:
-            if picture.index not in path:
-                path1 = path
-                if picture.index in pictureindices:
-                    path1.append(picture.index)
-                    for person in picture.people:
-                        indices = []
-                        for pic in globalpeople[peoplelist.index(person)].pictures:
-                            indices.append(pic)
-                        if person == p2:
-                            print("Found!")
-                            return path1, loopnum
-                        elif person not in peoplepath:
-                            return search(indices,person,p2,path1,loopnum,peoplepath)
+def subsearch(personindex, p2):
+    people = []
+    for pic in globalpeople[personindex].pictures:
+        for person in globalpictures[pic].people:
+            people.append(peoplelist.index(person))
+            if person == p2:
+                return people, True
+    return people, False
+
+globalpaths = []
+searchedpeople = []
+
+def search(p1,p2,loopnum):
+    
+    found = False
+    loopnum+=1
+
+    print("Loop: ",str(loopnum))
+
+    if loopnum == 1:
+        globals()[f'path{str(loopnum)}'] = []
+        globals()[f'path{str(loopnum)}'].append(p1)
+        searchedpeople.append(p1)
+        globalpaths.append(globals()[f'path{str(loopnum)}'])
+
+    elif loopnum > 1 and loopnum < 12:
+        searchpaths = []
+        for paths in globalpaths:
+            if len(paths) == loopnum - 1:
+                searchpaths.append(paths)
+            elif len(paths) < loopnum - 1:
+                del paths
+        for path in searchpaths:
+            newpeople, found = subsearch(path[loopnum-2],p2)
+            for i,p in enumerate(newpeople):
+                if p not in path and p not in searchedpeople:
+                    searchedpeople.append(p)
+                    globals()[f'path{str(loopnum)}|{str(i)}'] = path.copy()
+                    globals()[f'path{str(loopnum)}|{str(i)}'].append(p)
+                    globalpaths.append(globals()[f'path{str(loopnum)}|{str(i)}'])
+                    if p == p2:
+                        return globals()[f'path{str(loopnum)}|{str(i)}'], loopnum
+    
+    elif loopnum == 12:
+        print("Error: 12+ people away")
+
+    else:
+        print("Error")
+    
+    if not found:
+        return search(p1,p2,loopnum)
 
 #run
 
@@ -116,20 +149,39 @@ for p in globalpeople:
     elif p.name == Person2:
         p2 = True
 
+from PIL import Image
+
 foundpath = []
 loops = 0
 
+per1 = 0
+per2 = 0
+
 if p1 and p2:
+    foundpath = []
+    peoplepath = []
     for person in globalpeople:
-        if person.name==Person1:
-            foundpath, loops = search(person.pictures,Person1,Person2,[],0,[])
+        if person.name==Person2:
+            per2 = person.index
+        elif person.name==Person1:
+            per1 = person.index
+    ppath, loops = search(per1,per2, 0)
+    
+    for i,person in enumerate(ppath):
+        peoplepath.append(person)
+        if i < len(ppath) - 1:
+            for pic in globalpeople[person].pictures:
+                if peoplelist[ppath[i+1]] in globalpictures[pic].people:
+                    foundpath.append(pic)
+                    break
+
+    print(foundpath)
+    print("Connection found from "+Person1+" to "+Person2+" in "+str(loops)+" steps.")
+    print("\nThrough: \n")
+    for p in peoplepath:
+        print(globalpeople[p].name)
+    for pic in foundpath:
+        img = Image.open(globalpictures[pic].path)
+        img.show() 
 else:
     print("Person not found")
-
-from PIL import Image
-
-if foundpath != []:
-    print("Connection found from "+Person1+" to "+Person2+" in "+str(loops)+" steps.")
-    for i,v in enumerate(foundpath):
-        img = Image.open(globalpictures[i].path)
-        img.show() 
